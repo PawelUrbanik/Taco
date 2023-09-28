0bf390bdbc64e8de8982418b78990ee91d046db4
Pawel <pawelurbanik94@gmail.com>
2023-09-28 08:23:22 +0200

    Added order list for user

--- src/main/java/pl/springinaction/tacocloud/controller/OrdersController.java
+++ src/main/java/pl/springinaction/tacocloud/controller/OrdersController.java
@@ -1,6 +1,9 @@
 package pl.springinaction.tacocloud.controller;
 
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,9 +11,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.support.SessionStatus;
 import pl.springinaction.tacocloud.model.Order;
 import pl.springinaction.tacocloud.model.User;
+import pl.springinaction.tacocloud.repository.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -18,6 +21,12 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/orders")
 public class OrdersController {
+
+    private OrderRepository orderRepository;
+
+    public OrdersController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model)
@@ -36,7 +45,16 @@ public class OrdersController {
             return "orderForm";
         }
         order.setUser(user);
+        orderRepository.save(order);
         log.info("Zamówienie zostało złożone" + order.getTacos());
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(Model model, @AuthenticationPrincipal User user) {
+        Pageable pageable = PageRequest.of(0, 20);
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
     }
 }
--- src/main/java/pl/springinaction/tacocloud/repository/OrderRepository.java
+++ src/main/java/pl/springinaction/tacocloud/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package pl.springinaction.tacocloud.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.springinaction.tacocloud.model.Order;
+import pl.springinaction.tacocloud.model.User;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
